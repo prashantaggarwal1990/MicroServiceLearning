46f32c1d46ea95662308b368c18958b8cd0cd76e
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2021-02-16 11:11:34 +0530

    Commit First Push

---
+++ MicroRabbit.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MicroRabbit.Mvc.Models;
+using MicroRabbit.Mvc.Models.DTO;
+using MicroRabbit.Mvc.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDTO transferDto = new TransferDTO()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+
+            return View("Index");
+        }
+    }
+}
---
+++ MicroRabbit.Mvc/Services/ITransferService.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Mvc.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Mvc.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDTO transferDTO);
+    }
+}
